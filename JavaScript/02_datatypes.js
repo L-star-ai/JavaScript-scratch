1af304c89384e0f54828760d7f087e1ace492d16
L-star-ai <77183084+L-star-ai@users.noreply.github.com>
2023-09-27 12:48:59 +0000

    02

---
+++ JavaScript/02_datatypes.js
@@ -0,0 +1,15 @@
+let a = 1.00
+let fname = "lisma"
+let isLogIn = true
+let age = null
+let father = undefined
+
+console.table([a, fname, isLogIn, age, father])
+console.log(typeof a);
+console.log(typeof fname);
+console.log(typeof isLogIn);
+console.log(typeof age);
+console.log(typeof father);
+
+// typeof null => object
+// typeof undefined => undefined
